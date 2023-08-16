7ec934aadd694fda214bd120769ed0600e136050
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-08-16 16:32:22 +0300

    units 96-98

--- script.js
+++ script.js
@@ -1162,19 +1162,58 @@ on some row*/
 
   const secondTree = myPlants[1].list[1];
 }
-////////////////////////////// unit 90 /////////////////////////////////
-{
+////////////////////////////// unit 96 /////////////////////////////////
+// {
+//   // Налаштування
+//   const recordCollection = {
+//     2548: {
+//       albumTitle: 'Slippery When Wet',
+//       artist: 'Bon Jovi',
+//       tracks: ['Let It Rock', 'You Give Love a Bad Name']
+//     },
+//     2468: {
+//       albumTitle: '1999',
+//       artist: 'Prince',
+//       tracks: ['1999', 'Little Red Corvette']
+//     },
+//     1245: {
+//       artist: 'Robert Palmer',
+//       tracks: []
+//     },
+//     5439: {
+//       albumTitle: 'ABBA Gold'
+//     }
+//   };
 
-}
-////////////////////////////// unit 90 /////////////////////////////////
-{
+//   // Змініть код лише під цим рядком
+//   function updateRecords(records, id, prop, value) {
+//     records.id.prop = value;
+//     return records;
+//   }
 
+//   updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 }
-////////////////////////////// unit 90 /////////////////////////////////
+////////////////////////////// unit 97 /////////////////////////////////
 {
-
+  // Налаштування
+  const myArray = [];
+  let i = 5;
+  while (i >= 0) {
+    myArray.push(i);
+    i--;
+  }
+  // Змініть код лише під цим рядком
 }
-////////////////////////////// unit 90 /////////////////////////////////
+////////////////////////////// unit 98 /////////////////////////////////
+{
+  // Налаштування
+  const myArray = [];
+  for (let i = 1; i <= 5; i++) {
+    myArray.push(i);
+  }
+  // Змініть код лише під цим рядком
+}
+////////////////////////////// unit 99 /////////////////////////////////
 {
 
 }
