4baf898303bf5e1c8ab8e7f31349049dc260de40
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-08-17 16:22:19 +0300

    units 111 -112

--- script.js
+++ script.js
@@ -1372,11 +1372,21 @@ on some row*/
 }
 ////////////////////////////// unit 111 /////////////////////////////////
 {
+  function checkEqual(a, b) {
+    return a == b ? "Equal" : "Not Equal";
+  }
 
+  checkEqual(1, 2);
 }
 ////////////////////////////// unit 112 /////////////////////////////////
 {
+  function checkSign(num) {
+    return (num > 0) ? "positive"
+      : (num < 0) ? "negative"
+        : "zero";
+  }
 
+  checkSign(10);
 }
 ////////////////////////////// unit 113 /////////////////////////////////
 {
