5dfb6b96088ce4824fe849df1d143f6856f0be6a
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-08-18 18:28:46 +0300

    unit 96 test

--- test.js
+++ test.js
@@ -3,48 +3,48 @@ log = console.log;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // // unit  96 Налаштування
-// const recordCollection = {
-//   2548: {
-//     albumTitle: 'Slippery When Wet',
-//     artist: 'Bon Jovi',
-//     tracks: ['Let It Rock', 'You Give Love a Bad Name']
-//   },
-//   2468: {
-//     albumTitle: '1999',
-//     artist: 'Prince',
-//     tracks: ['1999', 'Little Red Corvette']
-//   },
-//   1245: {
-//     artist: 'Robert Palmer',
-//     tracks: []
-//   },
-//   5439: {
-//     albumTitle: 'ABBA Gold'
-//   }
-// };
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+log(recordCollection[5439].albumTitle);
+//Змініть код лише під цим рядком
 
-// // Змініть код лише під цим рядком
-// function updateRecords(records, id, prop, value) {
-//   if (value == "") {
-//     log(records[id][prop]);
-//     delete records[id][prop];
+function updateRecords(records, id, prop, value) {
+  if (value == "") {
+    log(records[id][prop]);
+    delete records[id][prop];
+  }
+  if (prop == 'tracks' && value != '') {
+    //log(records[id][prop] = value);
+    records[id][prop] = value;
+  }
+  // else {
+  //   records[id][prop] = value;
+  // }
+  return records;
+}
 
-//   }
-//   //records[id][prop] = value;
-//   //log(records[id]);
-//   return records;
-// }
+//updateRecords(recordCollection, 5439, 'artist', '');
+log(updateRecords(recordCollection, 5439, "artist", "ABBA")); +
 
-// //updateRecords(recordCollection, 5439, 'artist', '');
-// //log(updateRecords(recordCollection, 5439, 'artist', ''));
-
-// log(updateRecords(recordCollection, 2468, "tracks", "Free"));
+  log(updateRecords(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// unit 104
-// function sum(arr, n) {
-//   // Змініть код лише під цим рядком
 
-//   // Змініть код лише над цим рядком
-// }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
